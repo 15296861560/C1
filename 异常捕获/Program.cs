6c37be524f59a6760c7861d6fc6134993c39bbcc
lgy <lgy-lgy-l@qq.com>
2019-11-02 23:04:44 +0800

    异常捕获

---
+++ 异常捕获/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace 异常捕获
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //异常：语法上没有错误，在程序运行的过程当中，由于某些原因程序出现了错误，不能再正常的运行
+
+            /**
+             try{
+            可能出现异常的代码；
+            }
+            catch{
+            出现异常后要执行的代码；
+            }
+             **/
+
+
+            int a = 1, b = 0;
+            try
+            {
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch
+            {
+                Console.WriteLine("出现异常");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
