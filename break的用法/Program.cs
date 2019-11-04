63707adcd85ba93fb29e87ef4f05a836f7eab346
lgy <lgy-lgy-l@qq.com>
2019-11-04 21:48:35 +0800

    循环结构

---
+++ break的用法/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace break的用法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            //break作用：1.跳出switch-case结构
+            //2.跳出当前循环体
+            while (i<10)
+            {
+                i++;
+                if (i == 5) break;
+            }
+
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 循环结构/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace 循环结构
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            int i = n;
+            while (i>0)
+            {
+                i--;
+                Console.WriteLine("I'm while "+"i="+i);
+            }
+            i = n;
+            //do-while循环特点：循环体至少执行一遍
+            do
+            {
+                i--;
+                Console.WriteLine("I'm do-while " + "i=" + i);
+            } while (i != 0);
+
+            for (i = n; i > 0; i--)
+            {
+                Console.WriteLine("I'm for " + "i=" + i);
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
