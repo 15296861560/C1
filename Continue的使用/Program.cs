e70171cf76ccc9c1261b66969291d6902a2fdfaf
lgy <lgy-lgy-l@qq.com>
2019-11-05 22:47:04 +0800

    continue的使用

---
+++ Continue的使用/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Continue的使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // continue：不执行当前循环体的剩余内容，直接回到循环条件
+            int i = 0,j=0;
+            while (i<10)
+            {
+                i++;
+                if (i == 5)
+                {
+                    continue;
+                }
+                j++;
+            }
+
+
+            Console.WriteLine("i="+i+"j="+j);
+        }
+    }
+}
