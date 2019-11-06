6d2a556442406db0c4017796320601e97df80d10
lgy <lgy-lgy@qq.com>
2019-11-06 22:58:23 +0800

    for循环嵌套

---
+++ for循环嵌套/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace for循环嵌套
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            //把for循环整体看成单个语句，{}省略
+            for (i= 0;i<2;i++)
+            for (j= 0;j<2;j++)
+                {
+                    Console.WriteLine("i="+i+"j="+j);
+                }
+            //标准嵌套
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("i=" + i + "j=" + j);
+                }
+            }
+                
+
+
+
+        }
+    }
+}
