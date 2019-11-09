9742d1a59b96c04ba259a444d2a40132c588eacc
lgy <lgy-lgy@qq.com>
2019-11-09 11:18:40 +0800

    随机数

---
+++ 生成随机数/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 生成随机数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //产生随机数
+            //1.创建能够产生随机数的对象
+            Random r = new Random();
+            //2.让产生随机数的对象调用方法来产生随机数
+            int rN = r.Next(1, 10);//取值范围是左闭右开区间【1，10），而且是int类型
+            double rD = r.NextDouble();//生成0.0到1.0之间的double类型随机数
+            for(int i = 0; i < 10; i++)
+            {
+                rN = r.Next(1, 10);
+                Console.WriteLine(rN);
+                rD = r.NextDouble();
+                Console.WriteLine(rD);
+            }
+           
+        }
+    }
+}
