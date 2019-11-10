5263ddb01a82a7e591fab9f280e8b998a1a965b9
lgy <lgy-lgy@qq.com>
2019-11-10 10:21:50 +0800

    常量

---
+++ 常量/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 常量
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            num = 2;//变量可以被重新赋值
+            const int cnum = 1;//常量不能被重新赋值
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
