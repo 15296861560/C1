1386352258499aaca5b561d840c29866963c29f3
lgy <lgy-lgy@qq.com>
2019-11-10 10:32:03 +0800

    枚举

---
+++ 枚举/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace 枚举
+{
+    public enum Gender
+    {
+        男,
+        女
+    }
+
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+
+            /**
+             * 语法：
+             * [public] enum 枚举名
+             * {
+             * 值1,
+             * 值2,
+             * 值3,
+             * ...
+             * }
+             * 
+             * public:访问修饰符。公开的，哪都可以访问
+             * */
+
+            Gender gender = Gender.女;
+            Console.WriteLine(gender);
+        }
+    }
+}
