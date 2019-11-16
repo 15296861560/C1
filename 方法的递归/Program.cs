96d2dc3b22bdc425e7004f8d945c0a885d345f7f
lgy <lgy-lgy@qq.com>
2019-11-16 11:07:01 +0800

    方法的递归

---
+++ 方法的递归/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 方法的递归
+{
+    class Program
+    {
+        //方法的递归就是方法自己调用自己
+        static void Main(string[] args)
+        {
+            Console.WriteLine(recursion(-4));
+            Console.WriteLine(recursion(2));
+        }
+
+        public static int recursion(int x)
+        {
+            x++;
+            if (x < 0) return recursion(x);
+            else return x ;
+        }
+    }
+}
