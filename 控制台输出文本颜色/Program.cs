bae7a4e33b6ab21fc42e17c1a5645c2286887dc8
lgy <lgy-lgy@qq.com>
2019-11-17 11:05:26 +0800

    控制台输出文本颜色

---
+++ 控制台输出文本颜色/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 控制台输出文本颜色
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;//控制台背景色
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Red;//接下来输出的文本颜色
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
