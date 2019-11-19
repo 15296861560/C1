abea6e5b5cb4fbb13f1c1d97e9c7e1b9559b84c6
lgy <lgy-lgy@qq.com>
2019-11-19 22:48:42 +0800

    类的基本语法

---
+++ 类的基本语法/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace 类的基本语法
+{
+    public class Person
+    {
+        public string name;
+        int age;
+
+
+        public void setAge(int newAge)
+        {
+            this.age = newAge;
+        }
+    }
+
+    class Program
+    {
+
+        /**
+         * 语法：
+         * 访问权限 [] class 类名{
+         * 
+         *  访问权限 数据类型 属性名；
+         *  
+         *  
+         *  访问权限 [static] 返回类型 方法名（参数列表）
+         *  {
+         *  } 
+         * 
+         * }
+         * 
+         * 
+         * 
+         * 
+         * 类是不占内存的，而对象是占用内存的
+         * 
+         * */
+
+        
+
+        static void Main(string[] args)
+        {
+            Person p ;//自定义类
+            p = new Person();//创建Person类的对象
+            p.name = "小p";
+            p.setAge(18);
+
+            Console.WriteLine(p.name);
+        }
+    }
+}
