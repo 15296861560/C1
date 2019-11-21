0b9b5accecaa712dbfabebefaa0f36a3578c8e0a
lgy <lgy-lgy@qq.com>
2019-11-21 22:51:02 +0800

    类的属性

--- 类的基本语法/Program.cs
+++ 类的基本语法/Program.cs
@@ -4,13 +4,32 @@ namespace 类的基本语法
 {
     public class Person
     {
-        public string _name;
-        int _age;
+        public string _name;//字段
 
-
-        public void setAge(int newAge)
+        public string Name//属性
         {
-            _age = newAge;
+            get { return _name; }//当你输出属性的值的时候会执行get方法
+            set { _name = value; }//当你给属性赋值的时候首先会执行set方法
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set 
+            {
+                if (value < 0 || value > 100)
+                {
+                    value = 0;
+                }
+                _age = value; 
+            }
+        }
+
+
+        public void incAge()//方法
+        {
+            _age ++;
         }
     }
 
@@ -21,7 +40,7 @@ namespace 类的基本语法
          * 语法：
          * 访问权限 [] class 类名{
          * 
-         *  访问权限 数据类型 属性名；
+         *  访问权限 数据类型 字段名；
          *  
          *  
          *  访问权限 [static] 返回类型 方法名（参数列表）
@@ -35,6 +54,15 @@ namespace 类的基本语法
          * 
          * 类是不占内存的，而对象是占用内存的
          * 
+         * Field字段，Method方法，Property属性
+         * 
+         * 
+         * 属性的本质就是get和set方法，属性不存值，只是起到一个过渡作用
+         * 既有get又有set称之为可读可写属性
+         * 只有get没有set称之为只读属性
+         * 没有get只有set称之为只写属性
+         * 
+         * 
          * */
 
         
@@ -44,9 +72,11 @@ namespace 类的基本语法
             Person p ;//自定义类
             p = new Person();//创建Person类的对象
             p._name = "小p";
-            p.setAge(18);
+            p.Age = 16;
+            p.incAge();
 
-            Console.WriteLine(p._name);
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Age);
         }
     }
 }
