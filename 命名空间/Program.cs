9fca562bd2af60e20bbcfba5ad4f3fa40ff4207d
lgy <lgy-lgy@qq.com>
2019-11-24 09:59:36 +0800

    命名空间

---
+++ 命名空间/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using 面向对象初级;
+
+namespace 命名空间
+{
+    class Program
+    {
+
+        /*
+         * 命名空间
+         * 1.可以认为类是属于命名空间的
+         * 
+         * 
+         * 在一个项目中引用另一个项目的类
+         * 1.添加引用
+         * 2.引用命名空间
+         * 
+         */
+
+        static void Main(string[] args)
+        {
+            Pet p = new Pet();
+            p.SayHello();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
