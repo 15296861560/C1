f909444cc76e6853923fc663ee12a0ec8a817dcd
lgy <lgy-lgy@qq.com>
2019-11-29 12:00:57 +0800

    集合之ArrayList

--- 集合之ArrayList/Program.cs
+++ 集合之ArrayList/Program.cs
@@ -24,12 +24,18 @@ namespace 集合之ArrayList
             list.RemoveRange(0,3);//根据下标移除一定范围内的元素
             list.Sort();//升序排列（该集合内的类型要差不多）
             list.Reverse();//将集合内的元素反转
-            list.Insert(1, "插入的");//在指定位置插入元素
+            list.Insert(0, "插入的");//在指定位置插入元素
             list.InsertRange(0, vs);//在指定位置插入一个集合
             list.Contains(0);//判断是否包含某个指定元素
 
+            //count表示这个元素中实际包含的元素的个数
+            //capcity表示这个集合中可以包含的元素的个数
+            //每次集合中实际包含的元素个数count超过了可以包含元素的个数capcity的时候集合就会向内存申请多开辟一倍的空间，来保证集合的长度一直够用
 
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Capacity);
+            Console.ReadKey();
         }
     }
 }
