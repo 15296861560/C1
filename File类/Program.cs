66417be5a72947f22c8324246c77fa93bd10679a
lgy <lgy-lgy@qq.com>
2019-12-02 19:53:27 +0800

    File类

---
+++ File类/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace File类
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Create(@"D:\Test\c#\lgy.txt");//创建文件（如果该路径下有同名同类型文件将会删除原文件再创建）
+            //File.Delete(@"D:\Test\c#\new.txt");//删除指定目录下的指定文件
+            //File.Copy(@"D:\Test\c#\lgy.txt", @"D:\Test\c#\new.txt");//将指定目录下的指定文件复制到新位置，并使用新名称
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
