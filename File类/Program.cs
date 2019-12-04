800bafb429653255d6591a3684874d63eb2043b9
lgy <lgy-lgy@qq.com>
2019-12-04 11:33:45 +0800

    File类

--- File类/Program.cs
+++ File类/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace File类
 {
@@ -11,7 +12,14 @@ namespace File类
             //File.Delete(@"D:\Test\c#\new.txt");//删除指定目录下的指定文件
             //File.Copy(@"D:\Test\c#\lgy.txt", @"D:\Test\c#\new.txt");//将指定目录下的指定文件复制到新位置，并使用新名称
 
-            Console.WriteLine("Hello World!");
+            byte[] buffer = File.ReadAllBytes(@"D:\Test\c#\lgy.txt");//以字节为单位读取文件并存入字节数组
+
+            string s = Encoding.Default.GetString(buffer);//将字节数组中的每一个元素都要按照我们指定的编码格式解码成字符串
+
+            byte[] buf = Encoding.Default.GetBytes("谔谔阿德撒大大大苏打的");//将字符串转换成字节数组
+            File.WriteAllBytes(@"D:\Test\c#\new.txt", buf);//将字符串写入指定文件，并覆盖该文件的原本内容
+
+            Console.WriteLine(s);
         }
     }
 }
