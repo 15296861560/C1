b9ad4c556f64e544c1b95cbaec50d6f92a55a85e
lgy <lgy-lgy@qq.com>
2019-12-08 10:43:31 +0800

    StreamReader和StreamWriter

---
+++ StreamReader和StreamWriter的使用/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StreamReader和StreamWriter的使用
+{
+    class Program
+    {
+        //StreamReader和StreamWriter ：操作字符的
+
+        static void Main(string[] args)
+        {
+
+            string path = @"D:\Test\c#\lgy.txt";
+            //使用StreamReader读取文件
+            using (StreamReader sr = new StreamReader(path,Encoding.Default))
+            {
+                while (!sr.EndOfStream)//是否读到了文件的结尾
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+
+            //使用StreamWriter来写入一个文本文件
+            using (StreamWriter sw = new StreamWriter(path,true))
+            {
+                sw.Write("哈哈哈小孩子上学了！");
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
