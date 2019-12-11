eadb41919deb80b7d1323b2addad15807afe37e2
lgy <lgy-lgy@qq.com>
2019-12-11 10:20:06 +0800

    多态之抽象类

---
+++ 多态之抽象类/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace 多态之抽象类
+{
+
+    public abstract  class Pet
+    {
+       
+
+
+        public abstract void Yell();//抽象函数没有方法体
+        
+    }
+
+
+    public class Cat : Pet
+    {
+
+        public override  void Yell()//
+        {
+            Console.WriteLine("喵喵喵");
+        }
+
+    }
+
+    public class Dog : Pet
+    {
+        public override void Yell()
+        {
+            Console.WriteLine("汪汪汪");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet = new Cat();
+            Pet pet2 = new Dog();
+            pet.Yell();
+            pet2.Yell();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
