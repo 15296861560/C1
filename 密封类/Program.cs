f398b1a5ff81b8be2d129db8aa5141e16091c35a
lgy <lgy-lgy@qq.com>
2019-12-17 21:29:23 +0800

    密封类

---
+++ 密封类/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 密封类
+{
+    //密封类：不能够被其它类继承，但是可以继承其它类
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public sealed class P
+    {
+
+    }
+}
