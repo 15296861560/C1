802fcaea47d5b973747683a3fb8667114fe3fbcb
lgy <lgy-lgy@qq.com>
2020-01-09 09:53:37 +0800

    Socket客户端

---
+++ Socket客户端/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Socket客户端
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.txtMsg = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(58, 49);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(138, 25);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "192.168.137.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(245, 51);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(62, 25);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "50000";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(500, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(78, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "连接";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(58, 114);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(520, 166);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.Text = "";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(58, 325);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(520, 100);
+            this.txtMsg.TabIndex = 4;
+            this.txtMsg.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(469, 471);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 571);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.RichTextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Socket客户端/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket客户端
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //创建负责通信的Socket
+        Socket socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            IPAddress ip = IPAddress.Parse(txtServer.Text);
+            IPEndPoint endPoint = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+            //获得要连接的远程服务器应用程序的IP地址和端口号
+            socketSend.Connect(endPoint);
+            ShowMsg("连接成功");
+
+
+        }
+
+        /// <summary>
+        /// 显示信息
+        /// </summary>
+        /// <param name="v"></param>
+        void ShowMsg(string v)
+        {
+            txtLog.AppendText(v + "\r\n");
+        }
+        /// <summary>
+        /// 客户端给服务器发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string str = txtMsg.Text.Trim();
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            socketSend.Send(buffer);
+        }
+    }
+}
