aedf8692ca394c520894654ee81e1bd64a601890
lgy <lgy-lgy@qq.com>
2020-01-19 16:46:14 +0800

    单例模式

---
+++ 单例模式/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 单例模式
+{
+    /*
+     * 单例模式
+     * 1.将构造函数私有化
+     * 2.提供一个静态方法返回一个对象
+     * 3.创建一个单例
+     */
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 =Form2.GetSingle();
+            form2.Show();
+        }
+    }
+}
---
+++ 单例模式/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 单例模式
+{
+    public partial class Form2 : Form
+    {
+        //全局唯一的单例
+        public static Form2 formSingle=null;
+
+        private Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static Form2 GetSingle()
+        {
+            if (formSingle == null)
+            {
+                formSingle = new Form2();
+            }
+            return formSingle;
+        }
+    }
+}
