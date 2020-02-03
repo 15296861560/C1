13f66ced283b254b3ebad2a76b0146f03c1bdfe1
lgy <lgy-lgy@qq.com>
2020-02-03 13:29:07 +0800

    创建带属性的XML文档

---
+++ 创建带属性的XML文档/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml;
+
+namespace 创建带属性的XML文档
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(dec);
+
+            XmlElement order = doc.CreateElement("Order");
+            doc.AppendChild(order);
+
+            XmlElement customerName = doc.CreateElement("CustomerName");
+            customerName.InnerText = "渣渣辉";
+            order.AppendChild(customerName);
+
+            XmlElement customerNumber = doc.CreateElement("CustomerNumber");
+            customerNumber.InnerText = "100001";
+            order.AppendChild(customerNumber);
+
+
+            XmlElement items = doc.CreateElement("Items");
+            order.AppendChild(items);
+
+            XmlElement orderItem1 = doc.CreateElement("OrderItem");
+            //给节点添加属性
+            orderItem1.SetAttribute("Name", "可乐");
+            orderItem1.SetAttribute("Count", "10");
+            items.AppendChild(orderItem1);
+
+            XmlElement orderItem2 = doc.CreateElement("OrderItem");
+            //给节点添加属性
+            orderItem2.SetAttribute("Name", "雪碧");
+            orderItem2.SetAttribute("Count", "20");
+            items.AppendChild(orderItem2);
+
+
+            doc.Save("Order.xml");
+            Console.WriteLine("保存成功");
+            Console.ReadKey();
+        }
+    }
+}
